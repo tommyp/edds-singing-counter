40c614828c898c40cc63364da145eb1122f85790
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-20 13:42:30 +0100

    I can't code

--- app/controllers/clock_controller.rb
+++ app/controllers/clock_controller.rb
@@ -2,8 +2,8 @@ class ClockController < ApplicationController
 
   def index
     @clock = Clock.latest
-    if @clock.nil?
-      @clock = Clock.create!(start_time: Time.zone.now)
+    if @clock.blank?
+      @clock = Clock.create
     end
     @clocks = Clock.previous_clocks
   end
